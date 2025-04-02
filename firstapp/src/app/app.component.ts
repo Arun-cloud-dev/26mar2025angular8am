2b8a8294d22a9f45fb1d2fb9e10eda453bbb7b7f
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-02 08:49:14 +0530

    update string interpolation and property binging

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component'; 
+import { UserComponent } from './user/user.component';
+
 
 
 @Component({
   selector: 'app-ar',
-  imports: [HeaderComponent ],
+  imports: [HeaderComponent,UserComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'firstapp';
 
-
-  showMessage = true;
 }
