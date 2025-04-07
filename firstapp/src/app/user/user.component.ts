4e365d731c2726ced498d508625928da3c7b5417
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-07 08:42:14 +0530

    uptate

--- firstapp/src/app/user/user.component.ts
+++ firstapp/src/app/user/user.component.ts
@@ -1,26 +1,22 @@
-import { Component, Input, input} from '@angular/core';
-
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar();
+    return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-    console.log('Selected user: ' + this.name());
+    this.select.emit(this.id);
   }
 }
-
