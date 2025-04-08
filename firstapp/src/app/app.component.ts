6b87582349ced6f5bc616ede1154249ef4d0dfca
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-08 19:03:49 +0530

    udate

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component'; 
+import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-ar',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
-  onSelectUser(id:string) {
-    console.log('Selected user: ' + id);
-   }
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
