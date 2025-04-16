12e2ecf9bdd23c10ab34b138fa5d1d80f8f75f00
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-16 09:53:06 +0530

    update

--- firstapp/src/app/dummy-users.ts
+++ firstapp/src/app/dummy-users.ts
@@ -29,10 +29,18 @@ export const DUMMY_USERS = [
     name: 'Arjun Singh',
     avatar: 'user-6.jpg',
   },
-  ,
+
   {
     id: 'u7',
     name: 'A Singh',
     avatar: 'user-6.jpg',
   },
 ];
+
+
+
+// export const DUMMY_USERS = [
+//   { id: 'u1', name: 'Alice', avatar: 'user-1.jpg' },
+//   { id: 'u2', name: 'Bob', avatar: 'user-2.jpg' },
+//   // No `undefined` entries here
+// ];
